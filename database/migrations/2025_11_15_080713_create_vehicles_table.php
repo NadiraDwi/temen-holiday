5463bc1111aa9eb7b41d0e4baa616cd55f178097
asheralerca <ndwi4231@gmail.com>
2025-11-17 07:11:33 +0700

    first-commit

---
+++ database/migrations/2025_11_15_080713_create_vehicles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->char('id_vehicle', 36)->primary();
+            $table->string('nama_kendaraan');
+            $table->integer('kapasitas');
+            $table->string('gambar')->nullable();
+            $table->text('fasilitas')->nullable();
+            $table->decimal('harga', 12, 2);
+            
+            $table->char('id_contact', 36);
+            $table->foreign('id_contact')->references('id_contact')->on('contacts');
+
+            $table->char('updated_by', 36)->nullable();
+            $table->foreign('updated_by')->references('id_admin')->on('admins')
+                ->onDelete('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
---
+++ database/migrations/2025_11_15_080734_create_testimonials_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('testimonials', function (Blueprint $table) {
+            $table->char('id_testimoni', 36)->primary();
+            $table->string('nama_user');
+            $table->text('pesan');
+            $table->integer('rating');
+            $table->text('reply_admin')->nullable();
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('testimonials');
+    }
+};
